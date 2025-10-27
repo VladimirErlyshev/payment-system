54ff72f6a4a2a9eebb2ed5fdc3d1a6ff4629028a
dysangelium <vladimirerl@gmail.com>
2025-10-27 17:37:10 +0500

    introduce constant in jwt

--- payment-service-app/src/test/java/ru/verlyshev/configuration/TestJwtFactory.java
+++ payment-service-app/src/test/java/ru/verlyshev/configuration/TestJwtFactory.java
@@ -14,15 +14,22 @@ import java.util.stream.Stream;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class TestJwtFactory {
 
+    public static final String SUB = "sub";
+    public static final String PASSWORD = "123";
+    public static final String PREFERRED_USERNAME = "preferred_username";
+    public static final String REALM_ACCESS = "realm_access";
+    public static final String ROLES = "roles";
+    public static final String ROLE_PREFIX = "ROLE_";
+
     public static RequestPostProcessor jwtWithRole(String userName, String... roles) {
         return SecurityMockMvcRequestPostProcessors.jwt()
                 .jwt(jwt -> {
-                    jwt.claim("sub", "123");
-                    jwt.claim("preferred_username", userName);
-                    jwt.claim("realm_access", Map.of("roles", List.of(roles)));
+                    jwt.claim(SUB, PASSWORD);
+                    jwt.claim(PREFERRED_USERNAME, userName);
+                    jwt.claim(REALM_ACCESS, Map.of(ROLES, List.of(roles)));
                 })
                 .authorities(Stream.of(roles)
-                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                        .map(role -> new SimpleGrantedAuthority(ROLE_PREFIX + role))
                         .collect(Collectors.toList()));
     }
 }
